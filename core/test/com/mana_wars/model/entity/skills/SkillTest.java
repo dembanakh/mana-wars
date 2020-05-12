c2035c4ccc3e2dcb4c83b4722e0a0c3cb0337300
Artur Kasymov <art.kasymov@gmail.com>
2020-05-12 12:48:58 +0200

    init test config

---
+++ core/test/com/mana_wars/model/entity/skills/SkillTest.java
@@ -0,0 +1,22 @@
+package com.mana_wars.model.entity.skills;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SkillTest {
+
+    @org.junit.Test
+    public void getName() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void activate() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void getDescription() {
+    }
+}
