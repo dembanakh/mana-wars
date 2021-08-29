6206dc2cf9b5e6b688eac460973ea37a3ad7c8ef
Demian <dembanakh01@gmail.com>
2021-08-29 22:59:45 +0300

    add screenshots

--- android/src/com/mana_wars/utils/ApplicationConstants.java
+++ android/src/com/mana_wars/utils/ApplicationConstants.java
@@ -8,7 +8,7 @@ public final class ApplicationConstants {
     public static final SimpleDateFormat DEFAULT_APPLICATION_DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault());
     public static final int DB_VERSION = 1;
     public static final int INSTANCES_TO_UPDATE_FROM_JSON = 6;
-    public static final String DAILY_SKILLS_JSON_URL = "https://arturkasymov.student.tcs.uj.edu.pl/mana_wars_daily_skills.html";
+    public static final String DAILY_SKILLS_JSON_URL = "https://demianbanakh.student.tcs.uj.edu.pl/mana_wars_daily_skills.html";
 
     private ApplicationConstants() {}
 }
